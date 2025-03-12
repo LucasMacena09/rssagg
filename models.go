6f94e951531e03b3f7bb49df2d47d1aa452459d2
LucasMacena09 <macenamoreiralucas30@gmail.com>
2025-03-12 18:25:57 -0300

    feat: add posts table, queries, and scraper integration

--- models.go
+++ models.go
@@ -64,11 +64,11 @@ type FeedFollow struct {
 
 func databaseFeedFollowtoFeedFollow(dbFeedFollow database.FeedFollow) FeedFollow {
 	return FeedFollow{
-		ID: dbFeedFollow.ID,
+		ID:        dbFeedFollow.ID,
 		CreatedAt: dbFeedFollow.CreatedAt,
 		UpdatedAt: dbFeedFollow.UpdatedAt,
-		UserID: dbFeedFollow.UserID,
-		FeedID: dbFeedFollow.FeedID,
+		UserID:    dbFeedFollow.UserID,
+		FeedID:    dbFeedFollow.FeedID,
 	}
 }
 
@@ -78,4 +78,41 @@ func databaseFeedFollowstoFeedFollows(dbFeedFollows []database.FeedFollow) []Fee
 		feedFollows = append(feedFollows, databaseFeedFollowtoFeedFollow(dbFeedFollow))
 	}
 	return feedFollows
+}
+
+type Post struct {
+	ID          uuid.UUID `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Title       string    `json:"title"`
+	Description *string   `json:"description"`
+	PublishedAt time.Time `json:"published_at"`
+	Url         string    `json:"url"`
+	FeedID      uuid.UUID `json:"feed_id"`
+}
+
+func databasePostToPost(dbPost database.Post) Post {
+	var description *string
+	if dbPost.Description.Valid {
+		description = &dbPost.Description.String
+	}
+
+	return Post {
+		ID: dbPost.ID,
+		CreatedAt: dbPost.CreatedAt,
+		UpdatedAt: dbPost.UpdatedAt,
+		Title: dbPost.Title,
+		Description: description,
+		PublishedAt: dbPost.PublishedAt,
+		Url: dbPost.Url,
+		FeedID: dbPost.FeedID,
+	}
+}
+
+func databasePostsToPosts(dbPosts []database.Post) []Post {
+	posts := []Post{}
+	for _, dbPost := range dbPosts {
+		posts = append(posts, databasePostToPost(dbPost))
+	}
+	return posts
 }
